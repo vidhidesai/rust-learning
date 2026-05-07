5c85741018cdeccb31bd6375c679283ce5cbcf23
Vidhi Desai <vidhidesai09@gmail.com>
2026-05-07 12:44:12 +0530

    cargo update

---
+++ hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d
@@ -0,0 +1,5 @@
+/Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d: src/main.rs
+
+/Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/target/debug/hello_cargo: /Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/src/main.rs
