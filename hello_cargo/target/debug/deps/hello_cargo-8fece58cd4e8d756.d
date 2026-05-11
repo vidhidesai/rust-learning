0bfdf4d4dac0741efb0007c0360ffc01183ad284
Vidhi Desai <vidhidesai09@gmail.com>
2026-05-11 12:13:38 +0530

    Removed target and Cargo.lock

--- hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d
+++
@@ -1,5 +0,0 @@
-/Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756.d: src/main.rs
-
-/Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/target/debug/deps/hello_cargo-8fece58cd4e8d756: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/target/debug/hello_cargo: /Users/vidhidesai/workspace/learning/Rust/master/rust-learning/hello_cargo/src/main.rs
