1c6b27750eac2e25e41948cdaa05721f27ad6ab6
Vidhi Desai <vidhidesai09@gmail.com>
2026-05-12 10:40:51 +0530

    Commiting Ownership and Structure in rust

---
+++ ownership/src/bin/owner_move.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut s = String::from("hello");
+    s = String::from("ahoy");
+
+    println!("{s}, world!");
+
+}
---
+++ struct_in_rust/src/bin/struct_1.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+    println!("status:{}, username:{}, email:{}, count:{}", user1.active, user1.username, user1.email, user1.sign_in_count);
+    println!("{:?}", user1);
+}
